09d8a39509bc35664f3dfb53290cb9d613f29869
Meet <meet16123@gmail.com>
2024-07-25 10:40:27 -0400

    first commit

---
+++ backend/src/main/java/com/meet/contactAPI/ContactApiApplication.java
@@ -0,0 +1,11 @@
+package com.meet.contactAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ContactApiApplication.class, args);
+	}
+}
---
+++ backend/src/main/java/com/meet/contactAPI/service/ContactService.java
@@ -0,0 +1,77 @@
+package com.meet.contactAPI.service;
+
+import com.meet.contactAPI.domain.Contact;
+import com.meet.contactAPI.repo.ContactRepository;
+import jakarta.servlet.Servlet;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static com.meet.contactAPI.constant.Constant.PHOTO_DIRECTORY;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+@Service
+@Transactional(rollbackOn = Exception.class)
+@RequiredArgsConstructor
+@Slf4j
+public class ContactService {
+    private final ContactRepository contactRepository;
+
+    public Page<Contact> getAllContacts(int page, int size) {
+        return contactRepository.findAll(PageRequest.of(page, size, Sort.by("name")));
+    }
+
+    public Contact getContactById(String id) {
+        return contactRepository.findById(id).orElseThrow(() -> new RuntimeException("Contact not found"));
+    }
+
+    public Contact createContact(Contact contact) {
+        return contactRepository.save(contact);
+    }
+
+    public void deleteContact(Contact contact) {
+        contactRepository.deleteById(contact.getId());
+    }
+
+    public String uploadPhoto(String id, MultipartFile file) {
+        Contact contact = getContactById(id);
+         String photoUrl = photoFunction.apply(id, file);
+         contact.setPhotoUrl(photoUrl);
+         contactRepository.save(contact);
+         return photoUrl;
+
+    }
+
+    private final Function<String, String> fileExtension = filename -> Optional.of(filename).filter(name -> name.contains("."))
+            .map(name -> "." + name.substring(filename.lastIndexOf(".") + 1)).orElse(".png");
+
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        String filename = id + fileExtension.apply(image.getOriginalFilename());
+        try {
+            Path fileStorageLocation = Paths.get(PHOTO_DIRECTORY).toAbsolutePath().normalize();
+            if(!Files.exists(fileStorageLocation)) { Files.createDirectories(fileStorageLocation); }
+            Files.copy(image.getInputStream(), fileStorageLocation.resolve(filename), REPLACE_EXISTING);
+            return ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/contacts/image/" + filename).toUriString();
+        }catch (Exception exception) {
+            throw new RuntimeException("Unable to save image");
+        }
+    };
+
+
+}
